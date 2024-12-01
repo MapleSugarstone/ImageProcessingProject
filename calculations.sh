a5bd224aa86036d3f877080d3e72f50332af6734
Nathan Grimsey <ngrimsey@uw.edu>
2024-11-30 18:14:30 -0800

    Polished bash scripts

--- calculations.sh
+++ calculations.sh
@@ -5,8 +5,19 @@ SD=$(awk '{ total += $1; count++; array[NR] = $1 } END {mean = total / count; fo
 Min=$(sort -n ${1} | head -1)
 Max=$(sort -n ${1} | tail -1)
 
-echo "Mean: ${Mean}"
-echo "Min: ${Min}"
-echo "Max: ${Max}"
-echo "Range: $((Max - Min))"
-echo "Standard Deviation: ${SD}"
+YELLOW="\033[33m"
+UNCOLORED="\033[0m"
+
+if [ "${2}" = "-d" ]
+then
+	Mean=$(bc <<< "scale=3; $Mean / 1000.0" | awk '{printf "%.3f", $0}')
+	SD=$(bc <<< "scale=3; $SD / 1000.0" | awk '{printf "%.3f", $0}')	
+	Min=$(bc <<< "scale=3; $Min / 1000.0" | awk '{printf "%.3f", $0}')
+	Max=$(bc <<< "scale=3; $Max / 1000.0" | awk '{printf "%.3f", $0}')
+fi
+echo -e "      ${YELLOW}Mean:${UNCOLORED} ${Mean}"
+echo -e "      ${YELLOW}Min:${UNCOLORED} ${Min}"
+echo -e "      ${YELLOW}Max:${UNCOLORED} ${Max}"
+echo -e "      ${YELLOW}Range:${UNCOLORED} "$(bc <<< "$Max - $Min")
+echo -e "      ${YELLOW}Standard Deviation:${UNCOLORED} ${SD}"
+echo -e "      ${YELLOW}Coefficient of Variation:${UNCOLORED} "$(bc <<< "scale=5; $SD / $Mean * 100" | awk '{printf "%.3f", $0}')"%"
--- test.sh
+++ test.sh
@@ -2,6 +2,28 @@
 
 # Argument is amount of iterations
 
-./timefunction.sh "Process-Image" ${1} &
-./timefunction.sh "Python-Process-Image" ${1}
+# ./timefunction.sh "Process-Image" ${1} # &
+# ./timefunction.sh "Python-Process-Image" ${1}
 
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+UNCOLORED="\033[0m"
+
+echo -e "${RED}Process-Image (Java) stats:${UNCOLORED}"
+echo -e "${GREEN}  SMALL${UNCOLORED}"
+echo -e "${BLUE}    Runtime${UNCOLORED}"
+./calculations.sh times/Process-Image-small-runtimes -d
+echo -e "${BLUE}    Client Time${UNCOLORED}"
+./calculations.sh times/Process-Image-small-times
+echo -e "${GREEN}  MEDIUM${UNCOLORED}"
+echo -e "${BLUE}    Runtime${UNCOLORED}"
+./calculations.sh times/Process-Image-medium-runtimes -d
+echo -e "${BLUE}    Client Time${UNCOLORED}"
+./calculations.sh times/Process-Image-medium-times
+echo -e "${GREEN}  LARGE${UNCOLORED}"
+echo -e "${BLUE}    Runtime${UNCOLORED}"
+./calculations.sh times/Process-Image-large-runtimes -d
+echo -e "${BLUE}    Client Time${UNCOLORED}"
+./calculations.sh times/Process-Image-large-times
